d5216c03ad67150c5138fca0da7f7427080dc874
Daniel Akinniranye <dakinnir@Daniels-MacBook-Pro.local>
2026-03-02 14:00:36 -0500

    setup: initial commit

---
+++ src/test/java/com/dakinnir/urlshortenerservice/UrlShortenerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dakinnir.urlshortenerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
